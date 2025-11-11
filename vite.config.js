0fc6297c6cbecf21c85ccfe975dac6b534056bd2
ksanmartinm <kevin.gino.smm@gmail.com>
2025-11-11 04:52:46 -0300

    Correccion de Dockerinasion de laravel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // permite conexiones desde otros contenedores o localhost
+        port: 5173,
+        origin: 'http://localhost:5173', // evita uso de [::]
+        cors: true, // habilita CORS
+
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
